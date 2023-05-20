6a55cf21108387c94d36f8c40e627235c299bc0c
Sasha Kiselev <sdkiselev1812+github@gmail.com>
2023-05-20 17:56:54 +0200

    hello world server setup

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,21 @@
-fn main() {
-    println!("Hello Sasha");
+use actix_web::{web, App, HttpRequest, HttpServer, Responder};
+use anyhow::Result;
+
+async fn greet(req: HttpRequest) -> impl Responder {
+    let name = req.match_info().get("name").unwrap_or("World");
+    format!("Hello {name}")
+}
+
+#[tokio::main]
+async fn main() -> Result<()> {
+    HttpServer::new(|| {
+        App::new()
+            .route("/", web::get().to(greet))
+            .route("/{name}", web::get().to(greet))
+    })
+    .bind("127.0.0.1:8080")?
+    .run()
+    .await?;
+
+    Ok(())
 }
