c7b8c121e7aa105218349ac58b624c0aa6b58447
Sasha Kiselev <sdkiselev1812+github@gmail.com>
2023-05-20 19:22:34 +0200

    testing precommit

--- src/lib.rs
+++ src/lib.rs
@@ -7,10 +7,6 @@ async fn health_check() -> impl Responder {
 pub fn setup_server(
     socket_address: impl std::net::ToSocketAddrs,
 ) -> Result<Server, std::io::Error> {
-
-
-
-
     let server = HttpServer::new(|| {
         // Order in which routes are registered matters!
         App::new().route("/health_check", web::get().to(health_check))
