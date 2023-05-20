f6e0e6d4213b2923eea790ed333a3251aa06f017
Sasha Kiselev <sdkiselev1812+github@gmail.com>
2023-05-20 17:34:57 +0200

    setup precommit hooks

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,3 @@
-fn my_name() -> String {
-    "Sasha".to_string()
-}
-
 fn main() {
-    println!("Hello, {}!", my_name());
-}
-
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn test_my_name() {
-        assert_eq!(my_name(), "Sasha");
-    }
+    println!("Hello Sasha");
 }
